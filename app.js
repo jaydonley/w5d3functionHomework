0dc179ff809664910eb830a8c5f90abe44063226
Jason Donley <jay.donley@gmail.com>
2022-10-27 08:36:20 -0400

    w5d3hw

---
+++ app.js
@@ -0,0 +1,22 @@
+// ----------------- slice ---------------
+
+let str = "The quick brown fox jumps over the lazy dog.";
+
+
+console.log(str.slice(4, 19));
+
+
+// ----------------- splice ---------------
+
+let arr = ['one', 'two', 'three', '99'];
+
+
+
+arr.splice(3, 1);	
+
+// -------------- split --------------
+
+let myString = "I need to figure out how to split this up.";
+let myArr = myString.split(" ");
+
+console.log(myArr);
